71704005f5daed4972cf2798e35ac77f9706e804
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-07-16 14:51:24 -0500

    Create Jira Helper (#2)

---
+++ src/Services/Jira/JiraHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace EncoreDigitalGroup\Atlassian\Services\Jira;
+
+/** @api */
+class JiraHelper
+{
+    public function getKeyFromSmartLink(string $smartLink): ?string
+    {
+        $pattern = '/https:\/\/[a-zA-Z0-9.-]+\/browse\/([A-Z]+-\d+)|/';
+
+        if (preg_match($pattern, $smartLink, $matches)) {
+            return $matches[1];
+        }
+
+        return null;
+    }
+}
