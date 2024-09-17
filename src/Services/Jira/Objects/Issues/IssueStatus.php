72b4c423d2c6e189e21abcd6b506a0dbc6d7175b
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-09-17 13:08:51 -0500

    [Jira] Make All Jira Issue Fields Nullable (#29)

--- src/Services/Jira/Objects/Issues/IssueStatus.php
+++ src/Services/Jira/Objects/Issues/IssueStatus.php
@@ -9,9 +9,9 @@ namespace EncoreDigitalGroup\Atlassian\Services\Jira\Objects\Issues;
 
 class IssueStatus
 {
-    public string $self;
-    public string $description;
-    public string $iconUrl;
-    public string $name;
-    public string $id;
+    public ?string $self;
+    public ?string $description;
+    public ?string $iconUrl;
+    public ?string $name;
+    public ?string $id;
 }
