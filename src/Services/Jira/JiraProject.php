76d0bb71649ad9e139bccf9164a3f789f095e906
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-10-28 11:14:02 -0500

    [JQL] Trait Extraction

--- src/Services/Jira/JiraProject.php
+++ src/Services/Jira/JiraProject.php
@@ -7,14 +7,11 @@
 
 namespace EncoreDigitalGroup\Atlassian\Services\Jira;
 
-use EncoreDigitalGroup\Atlassian\AtlassianHelper;
-use EncoreDigitalGroup\Atlassian\Helpers\AuthHelper;
+use EncoreDigitalGroup\Atlassian\Services\Jira\Common\InteractsWithAtlassian;
 use EncoreDigitalGroup\Atlassian\Services\Jira\Objects\Issues\Issue;
 use EncoreDigitalGroup\Atlassian\Services\Jira\Objects\Issues\Traits\MapIssues;
 use EncoreDigitalGroup\Atlassian\Services\Jira\Objects\JQL\JqlResult;
 use EncoreDigitalGroup\Atlassian\Services\Jira\Objects\JQL\Traits\HandleJql;
-use PHPGenesis\Http\HttpClient;
-use PHPGenesis\Http\HttpClientBuilder;
 
 /**
  * @experimental
@@ -23,28 +20,11 @@ use PHPGenesis\Http\HttpClientBuilder;
  */
 class JiraProject
 {
+    use InteractsWithAtlassian;
     use HandleJql;
     use MapIssues;
 
     public const string ISSUE_ENDPOINT = '/rest/api/2/issue';
-
-    public function __construct(public string $hostname, public string $username, public string $token)
-    {
-        $this->hostname = $hostname ?: AtlassianHelper::getHostname();
-        $this->username = $username ?: AtlassianHelper::getUsername();
-        $this->token = $token ?: AtlassianHelper::getToken();
-
-        $builder = new HttpClientBuilder();
-    }
-
-    public static function make(?string $hostname = null, ?string $username = null, ?string $token = null): JiraProject
-    {
-        return new self(
-            $hostname ?: AtlassianHelper::getHostname(),
-            $username ?: AtlassianHelper::getUsername(),
-            $token ?: AtlassianHelper::getToken()
-        );
-    }
 
     public function getIssues(string $projectKey, int $startAt = 0, int $maxResults = 50): JqlResult
     {
@@ -53,16 +33,13 @@ class JiraProject
 
     public function createIssue(Issue $issue): Issue
     {
-        AuthHelper::validate($this);
-
         /** @var string $issueJson */
         $issueJson = json_encode($issue);
 
         /** @var array $issueArray */
         $issueArray = json_decode($issueJson, true);
 
-        $response = HttpClient::withBasicAuth($this->username, $this->token)
-            ->post($this->hostname . self::ISSUE_ENDPOINT, $issueArray);
+        $response = $this->client()->post($this->hostname . self::ISSUE_ENDPOINT, $issueArray);
 
         $response = json_decode($response->body());
 
@@ -72,10 +49,7 @@ class JiraProject
 
     public function getIssue(string $id): Issue
     {
-        AuthHelper::validate($this);
-
-        $response = HttpClient::withBasicAuth($this->username, $this->token)
-            ->get($this->hostname . self::ISSUE_ENDPOINT . '/' . $id);
+        $response = $this->client()->get($this->hostname . self::ISSUE_ENDPOINT . '/' . $id);
 
         $response = json_decode($response->body());
 
