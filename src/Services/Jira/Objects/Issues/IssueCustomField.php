8400d6e286bcd2b276888d1ebf2b5493e8bbf98d
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-10-29 11:30:45 -0500

    [Jira] Add Custom Field Support

---
+++ src/Services/Jira/Objects/Issues/IssueCustomField.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace EncoreDigitalGroup\Atlassian\Services\Jira\Objects\Issues;
+
+class IssueCustomField
+{
+    public ?string $name = null;
+    public mixed $value = null;
+}
