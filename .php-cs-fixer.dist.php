b7dc60d706e06852104e0d8f5be4ea87a81afb8c
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2026-02-08 19:38:26 -0600

    Remove Copyright Notices

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,5 @@
 <?php
 
-/*
- * Copyright (c) 2024. Encore Digital Group.
- * All Right Reserved.
- */
 
 use App\Fixer\ClassNotation\CustomControllerOrderFixer;
 use App\Fixer\ClassNotation\CustomOrderedClassElementsFixer;
--- src/Providers/AtlassianServiceProvider.php
+++ src/Providers/AtlassianServiceProvider.php
@@ -1,9 +1,5 @@
 <?php
 
-/*
- * Copyright (c) 2024. Encore Digital Group.
- * All Right Reserved.
- */
 
 namespace EncoreDigitalGroup\Atlassian\Providers;
 
